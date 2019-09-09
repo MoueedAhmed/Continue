ef41b262f3777e1e4ce59a9633ee74174f9b81d3
MoueedAhmed <moueedrao@gmail.com>
2019-09-09 17:46:07 +0500

    Update PolicyActivity

--- app/src/main/java/com/amoueed/continueapp/PolicyActivity.java
+++ app/src/main/java/com/amoueed/continueapp/PolicyActivity.java
@@ -2,15 +2,22 @@ package com.amoueed.continueapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.amoueed.continueapp.splashscreen.SplashScreenActivity;
 
 public class PolicyActivity extends AppCompatActivity {
 
     private WebView policy_webview;
+    private Button agree_btn;
+    private Button disagree_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +28,25 @@ public class PolicyActivity extends AppCompatActivity {
         policy_webview = findViewById(R.id.policy_webview);
         policy_webview.getSettings().setJavaScriptEnabled(true);
         policy_webview.loadUrl("file:///android_asset/policy.html");
+
+        agree_btn = findViewById(R.id.agree_btn);
+        agree_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(PolicyActivity.this, EnrollmentActivity.class);
+                startActivity(in);
+                finish();
+            }
+        });
+
+        disagree_btn = findViewById(R.id.disagree_btn);
+        disagree_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
