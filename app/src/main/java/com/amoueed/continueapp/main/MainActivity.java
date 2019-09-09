971f409501826ba79cc09edae72eff594151cbc8
MoueedAhmed <moueedrao@gmail.com>
2019-09-09 15:08:24 +0500

    Add Animation fadein in Splash Screen Image logo

--- app/src/main/java/com/amoueed/continueapp/main/MainActivity.java
+++ app/src/main/java/com/amoueed/continueapp/main/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setTitle("Continue");
+        setTitle("CoNTiNuE");
 
         if (savedInstanceState == null) {
             Fragment newFragment = new NotificationFragment();
--- app/src/main/java/com/amoueed/continueapp/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/amoueed/continueapp/splashscreen/SplashScreenActivity.java
@@ -1,10 +1,15 @@
 package com.amoueed.continueapp.splashscreen;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 import com.amoueed.continueapp.EnrollmentActivity;
@@ -34,14 +39,18 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         titleImage = findViewById(R.id.imageViewTitle);
         Glide.with(this).load(R.drawable.title_logo).into(titleImage);
+        Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+        titleImage.startAnimation(startAnimation);
 
-        mSplashHandler.sendEmptyMessageDelayed(SplashHandler.TIMER_EXPIRED, 2000);
+
+        mSplashHandler.sendEmptyMessageDelayed(SplashHandler.TIMER_EXPIRED, 3000);
     }
 
     class SplashHandler extends Handler {
         static final int TIMER_EXPIRED = 100;
         @Override
         public void handleMessage(Message msg) {
+            titleImage.setAnimation(null);
             switch (msg.what) {
                 case TIMER_EXPIRED:
                     if(mAuth.getCurrentUser()!=null){
