a38b219f5c50cd11ad336752f0f02648e1b44618
MoueedAhmed <moueedrao@gmail.com>
2019-09-24 14:03:45 +0500

    Add DateConverter.java, AppDatabase.java, WeekDao.java
    and test insert and read funtionality in TestActivity

--- app/src/main/java/com/amoueed/continueapp/TestActivity.java
+++ app/src/main/java/com/amoueed/continueapp/TestActivity.java
@@ -9,13 +9,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.os.Bundle;
+import android.view.View;
 
+
+import com.amoueed.continueapp.database.AppDatabase;
+import com.amoueed.continueapp.database.WeekEntry;
+
+import java.util.Date;
 
 import static androidx.recyclerview.widget.DividerItemDecoration.VERTICAL;
 
 public class TestActivity extends AppCompatActivity implements WeekAdapter.ItemClickListener {
     private RecyclerView mRecyclerView;
     private WeekAdapter mAdapter;
+
+    private AppDatabase mDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +36,26 @@ public class TestActivity extends AppCompatActivity implements WeekAdapter.ItemC
         DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
         mRecyclerView.addItemDecoration(decoration);
 
+        mDb = AppDatabase.getInstance(getApplicationContext());
+
     }
 
     @Override
     public void onItemClickListener(int itemId) {
 
+    }
+
+    public void b1Clicked(View view) {
+
+        Date date = new Date();
+        WeekEntry weekEntry = new WeekEntry("resource/path", 1, date);
+        mDb.weekDao().insertWeek(weekEntry);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter.setWeeks(mDb.weekDao().loadAllWeeks());
     }
 }
 
---
+++ app/src/main/java/com/amoueed/continueapp/database/WeekDao.java
@@ -0,0 +1,26 @@
+package com.amoueed.continueapp.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface WeekDao {
+
+    @Query("SELECT * FROM week ORDER BY week_num")
+    List<WeekEntry> loadAllWeeks();
+
+    @Insert
+    void insertWeek(WeekEntry weekEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateWeek(WeekEntry weekEntry);
+
+    @Delete
+    void deleteWeek(WeekEntry weekEntry);
+}
